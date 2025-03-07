eaa57a326079bef7da467ccb2c88f94eb6b71e33
Vaishnavi Reddy <balgurivaishnavireddy@gmail.com>
2025-03-07 21:58:00 +0530

    mini capstone committed

---
+++ drag-drop-list/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DragAndDrop from "./DragAndDrop/DragAndDrop";
+
+function App() {
+  return (
+    <div>
+      <DragAndDrop />
+    </div>
+  );
+}
+
+export default App;
